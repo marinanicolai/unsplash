2fcb59a76088b58a276112609252ec10ef5d0e07
marina <nicolaimarina@gmail.com>
2021-02-18 13:21:17 -0500

    displaying  the images

--- src/App.js
+++ src/App.js
@@ -10,77 +10,74 @@ const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
 
-  function Unsplash() {
-    const [loading, setLoading] = useState(false);
-    const [photos, setPhotos] = useState([]);
-    const [page, setPage] = useState(0);
-    const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
-    const fetchImages = async () => {
-      setLoading(true);
-      let url;
-      const urlPage = `&page${page}`;
-      const urlQuery = `&query=${query}`;
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page${page}`;
+    const urlQuery = `&query=${query}`;
 
-      if (query) {
-        url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
-      } else {
-        url = `${mainUrl}${clientID}${urlPage}`;
-      }
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
 
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
 
-        invariant(!data?.errors?.length, "Wrong access key");
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data?.results;
+        }
 
-        setPhotos((oldPhotos) => {
-          if (query && page === 1) {
-            return data?.results;
-          }
+        if (query) {
+          return [...oldPhotos, ...data.results];
+        }
 
-          if (query) {
-            return [...oldPhotos, ...data.results];
-          }
+        return [...oldPhotos, ...data];
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
-          return [...oldPhotos, ...data];
-        });
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        console.log(error);
-      }
-    };
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
 
-    useEffect(() => {
-      fetchImages();
-    }, [page]);
+  console.log(photos);
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-          <div className="photos-center">
-            {photos.map((photo) => {
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              return <Photo key={photo.id} {...photo} />;
-            })}
-          </div>
-        </header>
-      </div>
-    );
-  }
+        {
+          photos.map(photo => (
+            <img alt="" key={photo.id} src={photo.urls.thumb} />))
+        }
+      </header>
+    </div>
+  );
 }
+
 
 export default App;
